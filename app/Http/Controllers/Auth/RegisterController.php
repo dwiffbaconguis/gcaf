ee62c4ab00d3f87dccdb06400c7ae9e73b515b7c
dwiff <dbaconguis.noc88@gmail.com>
2020-08-24 18:39:36 +0800

    feature: Login and Registration with roles

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use App\Models\User;
-use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
@@ -64,10 +64,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        $user->roles()->attach(\App\Models\Role::where('name', 'user')->first());
+
+        return $user;
     }
 }
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $userRole = Role::where('name', 'user')->first();
+        $adminRole = Role::where('name', 'admin')->first();
+
+        $user = new User;
+        $user->name = 'Test User';
+        $user->email = 'test.user@gmail.com';
+        $user->password = bcrypt('test1234');
+        $user->save();
+
+        $user->roles()->attach($userRole);
+
+        $admin = new User;
+        $admin->name = 'Dwiff Baconguis';
+        $admin->email = 'dwiff.baconguis@gmail.com';
+        $admin->password = bcrypt('test1234');
+        $admin->save();
+
+        $admin->roles()->attach($adminRole);
+    }
+}
