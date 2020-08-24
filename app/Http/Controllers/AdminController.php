8aa173c6050cfacbfc4922fd4744c18ab04c026c
dwiff <dbaconguis.noc88@gmail.com>
2020-08-24 20:33:31 +0800

    feature: Creates constructor and index method.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        if ($request->user()->hasRole('admin')) {
+            return view('admin.dashboard');
+        }
+
+        if ($request->user()->hasRole('user')) {
+            return redirect('/home');
+        }
+    }
 }
