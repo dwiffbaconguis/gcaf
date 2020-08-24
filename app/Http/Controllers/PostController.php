fc60bc82b9f875590223238517bf854953b35687
dwiff <dbaconguis.noc88@gmail.com>
2020-08-24 20:24:19 +0800

    feature: Creates post method.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
@@ -17,5 +19,32 @@ class PostController extends Controller
     public function single(Post $post)
     {
         return view('posts.single', compact('post'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+            'user_id' => 'required',
+            'image' => 'required|mimes:jpeg,png,jpg,gif,svg',
+        ]);
+
+        $post = new Post;
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $name = Str::slug($request->title) . '.' . $image->getClientOriginalExtension();
+            $destinationPath = public_path('/uploads/posts');
+            $imagePath = $destinationPath . "/" . $name;
+            $image->move($destinationPath, $name);
+            $post->image = $name;
+        }
+
+        $post->user_id = $request->user_id;
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        return new PostResource($post);
     }
 }
